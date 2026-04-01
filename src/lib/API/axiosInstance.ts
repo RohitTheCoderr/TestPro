f0404f9422ab0717e32ffe763a0e79637473c1c7
Rohit Kumar <rohit.kumar@pelocal.com>
2026-04-01 16:40:51 +0530

    update code

--- src/lib/API/axiosInstance.ts
+++ src/lib/API/axiosInstance.ts
@@ -1,24 +1,20 @@
 import axios from "axios";
 import { store } from "../redux/store";
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// ✅ Automatically attach token from localStorage (or Redux if needed)
+// Automatically attach token from localStorage (or Redux if needed)
 axiosInstance.interceptors.request.use(
   (config) => {
     // const token =
     //   typeof window !== "undefined" ? localStorage.getItem("authToken") : null;
     // const token = useSelector((state: RootState) => state.auth.token) || null;
 
-    const token = store.getState().auth.token; // ✅ from redux store
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    const token = store.getState().auth.token; // from redux store
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
