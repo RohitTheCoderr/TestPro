1e2ec317025483d53d8810571d27297e507b9b37
Rohit Kumar <rohit.kumar@pelocal.com>
2026-04-30 13:12:53 +0530

    uninstalle

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { apiClient } from "@/lib/API/apiClient";
 import { Category, CategoryResponse } from "@/Interfaces";
